cce80ee180d6ee8cf52ca8ddc2ae6d236ae2f815
Kira Helberg <kira.helberg@gmail.com>
2019-05-18 11:02:37 -0400

    Added scraping code

--- server.js
+++ server.js
@@ -10,6 +10,10 @@ var mongoose = require("mongoose");
 
 var PORT = 3000;
 
+// Require axios and cheerio. This makes the scraping possible
+var axios = require("axios");
+var cheerio = require("cheerio");
+
 // Initialize Express
 var app = express();
 
@@ -20,3 +24,75 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Make public a static folder
 app.use(express.static("public"));
+
+// Connect to the Mongo DB
+mongoose.connect("mongodb://localhost/userdb", { useNewUrlParser: true });
+
+// Database configuration
+var databaseUrl = "scraper";
+var collections = ["scrapedData"];
+
+// Hook mongojs configuration to the db variable
+var db = mongojs(databaseUrl, collections);
+db.on("error", function(error) {
+  console.log("Database Error:", error);
+});
+
+// Main route (simple Hello World Message)
+app.get("/", function(req, res) {
+  res.send("Hello world");
+});
+
+// Retrieve data from the db
+app.get("/all", function(req, res) {
+  // Find all results from the scrapedData collection in the db
+  db.scrapedData.find({}, function(error, found) {
+    // Throw any errors to the console
+    if (error) {
+      console.log(error);
+    }
+    // If there are no errors, send the data to the browser as json
+    else {
+      res.json(found);
+    }
+  });
+});
+
+// Scrape data from one site and place it into the mongodb db
+app.get("/scrape", function(req, res) {
+  axios.get("https://news.ycombinator.com/").then(function(response) {
+    var $ = cheerio.load(response.data);
+
+    $(".title").each(function(i, element) {
+      var title = $(element)
+        .children("a")
+        .text();
+      var link = $(element)
+        .children("a")
+        .attr("href");
+
+      if (title && link) {
+        db.scrapedData.insert(
+          {
+            title: title,
+            link: link
+          },
+          function(err, inserted) {
+            if (err) {
+              console.log(err);
+            } else {
+              console.log(inserted);
+            }
+          }
+        );
+      }
+    });
+  });
+
+  res.send("Scrape Complete");
+});
+
+// Listen on port 3000
+app.listen(3000, function() {
+  console.log("App running on port 3000!");
+});
